e27fffab2b81bcb87f44140aee4e6aae1bdef4f2
yandri <yandrinurdiana@gmail.com>
2024-04-29 09:46:07 +0700

    company profile

---
+++ webcompanyprofile/index.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+	<title>CV. ANUGRAH PRIMA ABADI</title>
+	<meta content="" name="description">
+	<meta content="" name="keywords">
+
+	<!-- Favicons -->
+	<link href="assets/img/logo.png" rel="icon">
+	
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+	<!-- Vendor CSS Files -->
+	<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+	<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+	<link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+	<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+	<!-- Template Main CSS File -->
+	<link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<!-- ======= Header ======= -->
+	<header id="header" class="fixed-top ">
+		<div class="container d-flex align-items-center">
+
+
+			
+			<a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"><span style="color:white;font-size: 25px;margin-left: 20px;">CV. ANUGRAH PRIMA ABADI</span></a>
+
+			<nav id="navbar" class="navbar">
+				<ul>
+					<li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+					<li><a class="nav-link scrollto" href="#about">Tentang Kami</a></li>
+					<li><a class="nav-link scrollto" href="#visimisi">Visi & Misi</a></li>
+					<li><a class="nav-link scrollto" href="#produk">Produk</a></li>
+					
+				</ul>
+				<i class="bi bi-list mobile-nav-toggle"></i>
+			</nav>
+
+		</div>
+	</header><!-- End Header -->
+
+	
+	<section id="hero" class="d-flex align-items-center">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+					<span style="font-size: 45px;color: white;font-weight: 800;">CV. ANUGRAH PRIMA ABADI</span>
+					<h2>Memberikan layanan ke konsumen dengan jaminan keaslian produk, dan kelangsungan suplai yang 
+					lengkap, serta mendapatkan harga terbaik untuk kualitas produk yang lebih baik.</h2>
+					
+				</div>
+				<div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+					<img src="assets/img/header.png" class="img-fluid animated" alt="" style="max-width: 150% !important;margin-bottom: 150px;">
+				</div>
+			</div>
+		</div>
+
+	</section>
+
+	<main id="main">
+
+		<section id="about" class="about">
+			<div class="container" data-aos="fade-up">
+
+				<div class="section-title">
+					<h2>Tentang Kami</h2>
+				</div>
+
+				<div class="row content">
+					<div class="content">
+
+						<p>
+							CV. Anugrah Prima Abadi didirikan pada tahun 2006 di 
+							Palembang, Provinsi Sumatera Selatan yang mempunyai 
+							legalitas sebagai berikut :
+						</p>
+						<p>
+							<ol>
+								<li>Akte Pendirian Perseroan 2006</li>
+								<li>Akte Perubahan Anggaran Dasar 2007</li>
+								<li>Akte Perubahan Anggaran Dasar 2020</li>
+								<li>Akte Perubahan Anggaran Dasar 2022</li>
+								<li>Pendaftaran Menkumham 2020</li>
+								<li>Pendaftaran Menkumham 2022</li>
+								<li>SIUP (Surat Ijin Usaha Perdagangan) yang diterbitkan Lembaga OSS</li>
+								<li>NIB (Nomor Induk Berusaha) dari Lembaga OSS + Kode KBLI</li>
+								<li>Lokasi usaha / bidang usaha yang telah memenuhi komitmen yang diterbitkan melalui system OSS.</li>
+								<li>NPWP</li>
+								<li>SPPKP (Surat Pengukuhan Pengusaha Kena Pajak)</li>
+								<li>SKT (Surat Keterangan Terdaftar) NPWP</li>
+								<li>SKT (Surat Keterangan Domisili)</li>
+							</ol>
+						</p>
+
+
+
+					</div>
+					
+				</div>
+
+			</div>
+		</section>	
+		
+		
+		<section id="visimisi" class="why-us section-bg">
+			<div class="container-fluid" data-aos="fade-up">
+
+				<div class="row">
+
+					<div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+
+						<div class="content">
+							<h3><strong>VISI</strong></h3>
+							<p style="font-size: 20px !important;">
+								Menjadi supplier terbaik dan profesional di 
+								bidang penyedia lubricant, chemical, dan jasa 
+								pabrikasi untuk pasar industri dengan harga 
+								bersaing.
+							</p>
+						</div>
+
+						
+
+					</div>
+
+					<div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/visi.png");height: 250px;' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+				</div>
+
+			</div>
+		</section>
+
+		
+		<section id="why-us" class="why-us section-bg">
+			<div class="container-fluid" data-aos="fade-up">
+
+				<div class="row">
+
+					<div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-3">
+
+						<div class="content">
+							<h3><strong>MISI</strong></h3>
+							<p style="font-size: 20px !important;">
+								Menyediakan produk secara cepat dan tepat 
+								sesuai permintaan pelanggan.
+								Menciptakan kemitraan dan kerjasama yang 
+								berkesinambungan dengan memberikan 
+								kualitas terbaik kepada pelanggan
+							</p>
+						</div>
+
+						
+
+					</div>
+
+					<div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/misi.png");height: 250px;' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+				</div>
+				
+			</div>
+		</section>
+
+		<section id="why-us" class="why-us section-bg">
+			<div class="container-fluid" data-aos="fade-up">
+				<div class="row">
+
+					<div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+
+						<div class="content">
+							<h3><strong>Values</strong></h3>
+							
+							<div><strong>Teamwork:</strong> Bekerjasama dengan efektif dan saling memberi masukan konstruktif.</div>
+							<div><strong>Kredibilitas Tinggi:</strong> Mampu memberikan kemampuan yang terbaik secara konsisten.</div>
+							<div><strong>Integritas:</strong> Menjalankan pekerjaan dan wewenang dengan penuh tanggung jawab tanpa melakukan tindak kecurangan.</div>
+
+
+							<div><strong>Loyalitas:</strong> Memiliki kesetiaan terhadap para konsumen.</div>
+							<div><strong>Berpikir Terbuka:</strong> Mau menerima ide-ide baru dan mampu mengembangkannya.</div>
+							
+
+						</div>
+
+						
+
+					</div>
+
+					<div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/values.png");height: 250px;' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+				</div>
+				
+			</div>
+
+
+		</div>
+	</section>
+
+	
+	<section id="produk" class="portfolio">
+		<div class="container" data-aos="fade-up">
+
+			<div class="section-title">
+				<h2>PRODUK</h2>
+				
+			</div>
+
+
+
+			<div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+				
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-card">
+					<div class="portfolio-img"><img src="assets/img/produk1.jpg" style="height: auto;" class="img-fluid" alt=""></div>
+					<div class="portfolio-info">
+						<h4>Produk 1</h4>
+						
+						<a href="assets/img/produk1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Produk 1"><i class="bx bx-plus"></i></a>
+						<a href="#" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+					</div>
+				</div>
+
+				
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-card">
+					<div class="portfolio-img"><img src="assets/img/produk2.jpg" style="height: auto;" class="img-fluid" alt=""></div>
+					<div class="portfolio-info">
+						<h4>Produk 2</h4>
+						
+						<a href="assets/img/produk2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Produk 2"><i class="bx bx-plus"></i></a>
+						<a href="#" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-md-6 portfolio-item filter-card">
+					<div class="portfolio-img"><img src="assets/img/produk3.jpg" style="height: auto;" class="img-fluid" alt=""></div>
+					<div class="portfolio-info">
+						<h4>Produk 3</h4>
+						
+						<a href="assets/img/produk3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="Produk 3"><i class="bx bx-plus"></i></a>
+						<a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+					</div>
+				</div>
+
+
+
+			</div>
+
+		</div>
+	</section>
+
+	
+
+</main>
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+	
+
+	<div class="footer-top" style="background-color: #f3f5fa;">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-lg-3 col-md-7 footer-contact">
+					<h3>CV. ANUGRAH PRIMA ABADI</h3>
+					<p>JL Bhati LKMD No. 29 C, Rt. 45 Rw. 09, Kel. Bukit Sangkala, Kec. Kalidoni, Palembang, Sumatera Selatan<br>
+						<strong>Telp. / Fax :</strong> 0711–5701673 <br>
+						<strong>Mobile Phone:</strong> 0812 7848 77711 <br> 
+						<strong>WhatsApp : </strong>0812 3639 9217 <br>
+						<strong>Email : </strong> anprimaa11@gmail.com</p>
+
+					</div>
+
+					
+
+					<div class="col-lg-3 col-md-6 footer-links">
+						<h4>Useful Links</h4>
+						<ul>
+							<li><i class="bx bx-chevron-right"></i> <a href="#hero">Home</a></li>
+							<li><i class="bx bx-chevron-right"></i> <a href="#about">Tentang Kami</a></li>
+							<li><i class="bx bx-chevron-right"></i> <a href="#visimisi">Visi & Misi</a></li>
+							<li><i class="bx bx-chevron-right"></i> <a href="#produk">Produk</a></li>
+							
+						</ul>
+					</div>
+					<div class="col-lg-3 col-md-6 footer-links">
+						<h4>Useful Links</h4>
+						<ul>
+							<li><i class="bx bx-chevron-right"></i> <a href="#hero">Home</a></li>
+							<li><i class="bx bx-chevron-right"></i> <a href="#about">Tentang Kami</a></li>
+							<li><i class="bx bx-chevron-right"></i> <a href="#visimisi">Visi & Misi</a></li>
+							<li><i class="bx bx-chevron-right"></i> <a href="#produk">Produk</a></li>
+							
+						</ul>
+					</div>
+					<div class="col-lg-3 col-md-6 footer-links">
+						<h4>Useful Links</h4>
+						<ul>
+							<li><i class="bx bx-chevron-right"></i> <a href="#hero">Home</a></li>
+							<li><i class="bx bx-chevron-right"></i> <a href="#about">Tentang Kami</a></li>
+							<li><i class="bx bx-chevron-right"></i> <a href="#visimisi">Visi & Misi</a></li>
+							<li><i class="bx bx-chevron-right"></i> <a href="#produk">Produk</a></li>
+							
+						</ul>
+					</div>
+					
+
+				</div>
+			</div>
+		</div>
+
+		<div class="container footer-bottom clearfix">
+			<div class="copyright">
+				&copy; Copyright <strong><span>CV. ANUGRAH PRIMA ABADI</span></strong>. All Rights Reserved
+			</div>
+			<div class="credits">
+				
+			</div>
+		</div>
+	</footer><!-- End Footer -->
+
+	<div id="preloader"></div>
+	<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+	<!-- Vendor JS Files -->
+	<script src="assets/vendor/aos/aos.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+	<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+	<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+	<script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+	<script src="assets/vendor/php-email-form/validate.js"></script>
+
+	<!-- Template Main JS File -->
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
