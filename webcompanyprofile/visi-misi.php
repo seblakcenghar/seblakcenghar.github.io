ec52b5953d4f27c1a393d4507ede375321844695
yandri <yandrinurdiana@gmail.com>
2024-05-03 22:54:10 +0700

    change layout

---
+++ webcompanyprofile/visi-misi.php
@@ -0,0 +1,94 @@
+<?php 
+include('header.php');
+?>
+
+<section id="visimisi" class="why-us section-bg">
+	<div class="container-fluid" data-aos="fade-up">
+
+		<div class="row">
+
+			<div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+
+				<div class="content">
+					<h3><strong>VISI</strong></h3>
+					<p style="font-size: 20px !important;">
+						Menjadi supplier terbaik dan profesional di 
+						bidang penyedia lubricant, chemical, dan jasa 
+						pabrikasi untuk pasar industri dengan harga 
+						bersaing.
+					</p>
+				</div>
+
+
+
+			</div>
+
+			<div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/visi.png");height: 250px;' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+		</div>
+
+	</div>
+</section>
+
+
+<section id="why-us" class="why-us section-bg">
+	<div class="container-fluid" data-aos="fade-up">
+
+		<div class="row">
+
+			<div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-3">
+
+				<div class="content">
+					<h3><strong>MISI</strong></h3>
+					<p style="font-size: 20px !important;">
+						Menyediakan produk secara cepat dan tepat 
+						sesuai permintaan pelanggan.
+						Menciptakan kemitraan dan kerjasama yang 
+						berkesinambungan dengan memberikan 
+						kualitas terbaik kepada pelanggan
+					</p>
+				</div>
+
+
+
+			</div>
+
+			<div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/misi.png");height: 250px;' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+		</div>
+
+	</div>
+</section>
+<section id="why-us" class="why-us section-bg">
+	<div class="container-fluid" data-aos="fade-up">
+		<div class="row">
+
+			<div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+
+				<div class="content">
+					<h3><strong>Values</strong></h3>
+
+					<div><strong>Teamwork:</strong> Bekerjasama dengan efektif dan saling memberi masukan konstruktif.</div>
+					<div><strong>Kredibilitas Tinggi:</strong> Mampu memberikan kemampuan yang terbaik secara konsisten.</div>
+					<div><strong>Integritas:</strong> Menjalankan pekerjaan dan wewenang dengan penuh tanggung jawab tanpa melakukan tindak kecurangan.</div>
+
+
+					<div><strong>Loyalitas:</strong> Memiliki kesetiaan terhadap para konsumen.</div>
+					<div><strong>Berpikir Terbuka:</strong> Mau menerima ide-ide baru dan mampu mengembangkannya.</div>
+
+
+				</div>
+
+
+
+			</div>
+
+			<div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/values.png");height: 250px;' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+		</div>
+
+	</div>
+
+
+</div>
+</section>
+<?php 
+include('footer.php');
+?>
