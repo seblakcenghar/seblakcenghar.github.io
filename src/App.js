04a0830b37d602253c846b1ef7ee03dc85d17855
yandri <yandrinurdiana@gmail.com>
2025-04-20 18:01:59 +0700

    initial react landing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    // Fetch file readme.md dari folder public
+    fetch('/readme.md')
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text))
+      .catch((err) => console.error('Failed to load markdown:', err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}>
+      {/* Render markdown sebagai HTML */}
+      <ReactMarkdown>{markdown}</ReactMarkdown>
     </div>
   );
 }
