0c5ef83c7a9890454bc3e60bd01d3c5587e6018f
yandri <yandrinurdiana@gmail.com>
2025-04-20 17:11:11 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    fetch('/readme.md')
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}>
+      <ReactMarkdown>{markdown}</ReactMarkdown>
     </div>
   );
 }
